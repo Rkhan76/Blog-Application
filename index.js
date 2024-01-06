bf55f6060f297ebeda15a2fcb23ce02a84623b66
Rakhshan khan <rakshaankhan1@gmail.com>
2024-01-06 23:40:16 +0530

    Add blog route and its functionality

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const path = require('path')
-const userRoute = require('./routes/user')
 const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
 const { checkForAuthenticationCookie } = require('./middlewares/authentication')
+
+const Blog = require('./models/blog')
+
+const userRoute = require('./routes/user')
+const blogRoute = require('./routes/blog')
 
 const app = express()
 const port = 3000
@@ -20,11 +24,14 @@ mongoose
     app.use(express.urlencoded({ extended: false }))
     app.use(cookieParser())
     app.use(checkForAuthenticationCookie('token'))
+    app.use(express.static(path.resolve('./public')))
 
     app.use('/user', userRoute)
+    app.use('/blog', blogRoute)
 
-    app.get('/', (req, res) => {
-      res.render('home', { user: req.user })
+    app.get('/', async (req, res) => {
+      const allBlogs = await Blog.find({})
+      res.render('home', { user: req.user, blogs: allBlogs })
     })
 
     app.listen(port, () => console.log(`Server is running on ${port}`))
--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,22 +1,25 @@
-const { validateToken } = require("../services/authentication")
+const { validateToken } = require('../services/authentication')
 
-function checkForAuthenticationCookie(cookieName){
-    console.log(cookieName)
-    return (req, res, next)=>{
-        const tokenCookieValue = req.cookies[cookieName]
-        if(!tokenCookieValue){
-            return next()
-        }
+function checkForAuthenticationCookie(cookieName) {
+  console.log(cookieName)
+  return (req, res, next) => {
+    const tokenCookieValue = req.cookies[cookieName]
 
-        try{
-            const userPayload = validateToken(tokenCookieValue)
-            req.user = userPayload
-        }catch(error){}
-       return next()
+    if (!tokenCookieValue) {
+      return next()
     }
+
+    try {
+      const userPayload = validateToken(tokenCookieValue)
+      req.user = userPayload
+    } catch (error) {
+      console.error('Error decoding token:', error)
+      // Handle the error appropriately, for example, clear the invalid token from cookies
+      res.clearCookie(cookieName)
+    }
+
+    return next()
+  }
 }
 
-module.exports = {
-    checkForAuthenticationCookie,
-}
-
+module.exports = {checkForAuthenticationCookie}
